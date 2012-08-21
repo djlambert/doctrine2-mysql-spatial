ffcb5ae0fc158033a71423adf1f9d65f33916dec
Derek Lambert <dlambert@dereklambert.com>
2012-08-20 20:04:25 -0500

    Accept coordinates in d:m:s format.

--- lib/CrEOF/PHP/Types/Point.php
+++ lib/CrEOF/PHP/Types/Point.php
@@ -18,14 +18,47 @@ class Point
     protected $longitude;
 
     /**
+     * @param mixed $position
+     *
+     * @return double
+     * @throws \Exception
+     */
+    protected function toDouble($position)
+    {
+        if (strpos($position, ':') === false) {
+            return (double) $position;
+        } else {
+            $found = preg_match_all('/^(?:(?:([0-8]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])|(90):(0?0):(0?0))([NnSs])|(?:(0?[0-9]?[0-9]|1[0-7][0-9]):([0-5]?[0-9]):([0-5]?[0-9])|(180):(0?0):(0?0))([EeWw]))$/', $position, $matches, PREG_SET_ORDER);
+
+            if ($found != 1) {
+                throw new \Exception();
+            }
+            list( , $degrees, $minutes, $seconds, $direction) = array_values(array_filter($matches[0]));
+
+            $position = $degrees + ((($minutes * 60) + $seconds) / 3600);
+
+            switch (strtolower($direction)) {
+                case 's':
+                case 'w':
+                    return (double) ($position * -1);
+                    break;
+                case 'n':
+                case 'e':
+                    return (double) $position;
+                    break;
+            }
+        }
+    }
+
+    /**
      * @param mixed $latitude
      * @param mixed $longitude
      */
     public function __construct($latitude = null, $longitude = null)
     {
         if ($latitude && $longitude) {
-            $this->latitude = (double)$latitude;
-            $this->longitude = (double)$longitude;
+            $this->latitude = $this->toDouble($latitude);
+            $this->longitude = $this->toDouble($longitude);
         }
     }
 
@@ -36,7 +69,7 @@ class Point
      */
     public function setLatitude($latitude)
     {
-        $this->latitude = (double)$latitude;
+        $this->latitude = $this->toDouble($latitude);
         return $this;
     }
 
@@ -55,7 +88,7 @@ class Point
      */
     public function setLongitude($longitude)
     {
-        $this->longitude = (double)$longitude;
+        $this->longitude = $this->toDouble($longitude);
         return $this;
     }
 
