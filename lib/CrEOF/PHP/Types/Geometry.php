b1ae1752f04d65d56601136d4a2d3b2d77cae0b8
Derek Lambert <dlambert@dereklambert.com>
2012-09-27 11:35:33 -0500

    Add common validation methods.

--- lib/CrEOF/PHP/Types/Geometry.php
+++ lib/CrEOF/PHP/Types/Geometry.php
@@ -2,10 +2,39 @@
 
 namespace CrEOF\PHP\Types;
 
+use CrEOF\Exception\InvalidValueException;
+
 /**
  * Base geometry object
  */
 abstract class Geometry
 {
+    const ARRAY_VALUE = 1;
+    const POINT_VALUE = 2;
 
+    protected function validatePoint($point, $type = null)
+    {
+        if ($type && $type != self::POINT_VALUE) {
+            throw InvalidValueException::mixedValues();
+        }
+
+        if (!($point instanceof Point)) {
+            throw InvalidValueException::valueNotPoint();
+        }
+
+        return self::POINT_VALUE;
+    }
+
+    protected function validatePointArray(array $array, $type = null)
+    {
+        if ($type && $type != self::ARRAY_VALUE) {
+            throw InvalidValueException::mixedValues();
+        }
+
+        foreach ($array as $point) {
+            $this->validatePoint($point);
+        }
+
+        return self::ARRAY_VALUE;
+    }
 }
