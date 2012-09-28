876e161adf840a187edffcc25d58e9ffd0f35085
Derek Lambert <dlambert@dereklambert.com>
2012-09-28 13:47:21 -0500

    Added MBREqual DQL function and tests.

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Add MySQL spatial extension support to Doctrine2.
 * Polygon
 * MBRContains
 * MBRDisjoint
+* MBREqual
 * ...more to come
 
 Based on [jhartikainen/doctrine2-spatial](https://github.com/jhartikainen/doctrine2-spatial) and [Doctrine Docs](http://docs.doctrine-project.org/projects/doctrine-orm/en/latest/cookbook/advanced-field-value-conversion-using-custom-mapping-types.html)
--- tests/CrEOF/Tests/OrmTest.php
+++ tests/CrEOF/Tests/OrmTest.php
@@ -67,6 +67,7 @@ abstract class OrmTest extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->_em->getConfiguration()->addCustomNumericFunction('numinteriorrings', 'CrEOF\ORM\Query\AST\Functions\NumInteriorRings');
         $this->_em->getConfiguration()->addCustomNumericFunction('mbrcontains', 'CrEOF\ORM\Query\AST\Functions\MBRContains');
         $this->_em->getConfiguration()->addCustomNumericFunction('mbrdisjoint', 'CrEOF\ORM\Query\AST\Functions\MBRDisjoint');
+        $this->_em->getConfiguration()->addCustomNumericFunction('mbrequal', 'CrEOF\ORM\Query\AST\Functions\MBREqual');
         $this->_em->getConfiguration()->addCustomStringFunction('geomfromtext', 'CrEOF\ORM\Query\AST\Functions\GeomFromText');
         $this->_em->getConfiguration()->addCustomStringFunction('geometrytype', 'CrEOF\ORM\Query\AST\Functions\GeometryType');
         $this->_em->getConfiguration()->addCustomStringFunction('linestring', 'CrEOF\ORM\Query\AST\Functions\LineString');
