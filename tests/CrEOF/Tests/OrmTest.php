bfe32a72e98b4187856dc073fc04341bfe5ee3f6
Derek Lambert <dlambert@dereklambert.com>
2012-09-28 13:57:00 -0500

    Added DQL function MBRIntersects and tests.

--- tests/CrEOF/Tests/OrmTest.php
+++ tests/CrEOF/Tests/OrmTest.php
@@ -68,6 +68,7 @@ abstract class OrmTest extends \Doctrine\Tests\OrmFunctionalTestCase
         $this->_em->getConfiguration()->addCustomNumericFunction('mbrcontains', 'CrEOF\ORM\Query\AST\Functions\MBRContains');
         $this->_em->getConfiguration()->addCustomNumericFunction('mbrdisjoint', 'CrEOF\ORM\Query\AST\Functions\MBRDisjoint');
         $this->_em->getConfiguration()->addCustomNumericFunction('mbrequal', 'CrEOF\ORM\Query\AST\Functions\MBREqual');
+        $this->_em->getConfiguration()->addCustomNumericFunction('mbrintersects', 'CrEOF\ORM\Query\AST\Functions\MBRIntersects');
         $this->_em->getConfiguration()->addCustomStringFunction('geomfromtext', 'CrEOF\ORM\Query\AST\Functions\GeomFromText');
         $this->_em->getConfiguration()->addCustomStringFunction('geometrytype', 'CrEOF\ORM\Query\AST\Functions\GeometryType');
         $this->_em->getConfiguration()->addCustomStringFunction('linestring', 'CrEOF\ORM\Query\AST\Functions\LineString');
